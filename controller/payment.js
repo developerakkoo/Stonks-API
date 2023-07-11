6dcce7d67665a8c46d8d33c576747b228d9d92d1
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-07-11 16:31:04 +0530

    sakib

--- controller/payment.js
+++ controller/payment.js
@@ -7,27 +7,19 @@ const stripe = require('stripe')(key);
 // Create a new payment
 exports.payment = async (req, res) => {
     try {
-        const { product, routes } = req.body;
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items: [{
-                price_data: {
-                    currency: "INR",
-                    product_data: {
-                        name: product.name,
-                        images: [product.image],
-                    },
-                    unit_amount: product.amount * 100,
-                },
-                quantity: product.quantity,
-            }],
-            mode: "payment",
-            success_url: routes.success_url,
-            cancel_url: routes.cancel_url,
+        const { amount } = req.body;
+        // Create a PaymentIntent with the order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "inr",
+        automatic_payment_methods: {
+            enabled: true,
+        },
         });
-        res.json({ id: session.id });
+        res.status(200).json({orderId:paymentIntent.client_secret})
     } catch (error) {
         console.log(error);
+        res.status(500).json({message:error.message,status:"ERROR"});
     }
 
 }
