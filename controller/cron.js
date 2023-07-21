2fe3f26647ebf70fa88634c99da14e0481c177c2
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-07-21 15:58:48 +0530

    sakib

--- controller/cron.js
+++ controller/cron.js
@@ -4,26 +4,11 @@ const IO = require('../socket');
 const moment = require('moment');
 const cron = require('node-cron');
 
-
 cron.schedule('* * * * *',async () =>{
     try{
-        const id = moment().subtract(20,"day").format('DD-MM-YYYY')
-        const data =  await stock.find({ Date:id})
-        if (!data===[]){
-            console.log("Calls Not Found");
-        }
-        await stock.deleteMany({Date:id})
-        console.log('Previous Calls History Deleted');
-    }catch(error){
-    }
-})
-
-
-cron.schedule('* * * * *',async () =>{
-    try{
-        let marketCloseTime = '3:30 PM'
+        
         let currentTime = moment().format('LT'); 
-        if (marketCloseTime === currentTime)  {
+        if ('3:30 PM' === currentTime ||'3:31 PM'=== currentTime ||'3:32 PM'=== currentTime ||'3:33 PM'=== currentTime ||'3:34 PM'=== currentTime ||'3:35 PM'=== currentTime)  {
             // console.log('here');
             const calls = await noCall.find({isNoCall:false});
             // console.log(calls);
@@ -41,4 +26,20 @@ console.log('noCall:true');
         console.log(error);
     }
 })
+
+cron.schedule('* * * * *',async () =>{
+    try{
+        const id = moment().subtract(20,"day").format('DD-MM-YYYY')
+        const data =  await stock.find({ Date:id})
+        if (!data===[]){
+            console.log("Calls Not Found");
+        }
+        await stock.deleteMany({Date:id})
+        console.log('Previous Calls History Deleted');
+    }catch(error){
+    }
+})
+
+
+
         console.log('Previous Calls History Deleted');
