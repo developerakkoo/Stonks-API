407e9c2c3273808f3f41a639dc0f4e92cb692c7c
sakibdevlekar <sakibdevlekar96@gmail.com>
2023-07-29 14:14:39 +0530

    stock data fix

--- test1.js
+++ test1.js
@@ -99,20 +99,20 @@
 //   console.log(`Laptop server listening on port ${port}`);
 // });
 
-const io = require('socket.io-client');
-console.log('here')
-const socket = io('http://192.168.0.227:8000'); // Replace with the actual IP address of your laptop
+// const io = require('socket.io-client');
+// console.log('here')
+// const socket = io('http://192.168.0.227:8000'); // Replace with the actual IP address of your laptop
 
 
 
-socket.on('connect', () => {
-  console.log('Mobile connected to Socket.io');
-  socket.on('get:Nifty50', (text) => {
-    console.log('Received data:', text);
-    console.log('done');
-  });
-});
+// socket.on('connect', () => {
+//   console.log('Mobile connected to Socket.io');
+//   socket.on('get:Nifty50', (text) => {
+//     console.log('Received data:', text);
+//     console.log('done');
+//   });
+// });
 
-socket.on('disconnect', () => {
-  console.log('Mobile disconnected from Socket.io');
-});
+// socket.on('disconnect', () => {
+//   console.log('Mobile disconnected from Socket.io');
+// });
